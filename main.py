c8aa141ad7417e468d9855dc9c61e3cbef12fc71
KaSt94 <k.stanczyk94@gmail.com>
2024-01-14 15:38:59 +0100

    POPRAWKI_2

--- main.py
+++ main.py
@@ -20,30 +20,30 @@ import requests
 
 # WYKRYWANIE LUDZI NA ZDJĘCIU I POKAZYWANIE - ZE ZDJĘCIA W PROJEKCIE
 
-# initialize the HOG descriptor/person detector
+# # initialize the HOG descriptor/person detector
 # hog = cv2.HOGDescriptor()
 # hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
-
-# load image
+#
+# # load image
 # image = cv2.imread('plaza1.jpg')
 # image = cv2.resize(image, (1050, 650))
-
-# detect people in the image
+#
+# # detect people in the image
 # (rects, weights) = hog.detectMultiScale(image, winStride=(4, 4), padding=(8, 8), scale=1.05)
-
-# draw the bounding boxes
+#
+# # draw the bounding boxes
 # for (x, y, w, h) in rects:
 #     cv2.rectangle(image, (x, y), (x + w, y + h), (0, 0, 255), 2)
 #
 # print(f'Found {len(rects)} humans')
-
-# show the output images
+#
+# # show the output images
 # cv2.imshow("People detector", image)
 # cv2.waitKey(0)
 
 # ZWRACANIE INFORMACJI HTTPS - WYKRYWANIE LUDZI(POBRANE ZDJĘCIE Z NETA - URL ZASZYTY W KODZIE)
 
-# initialize the HOG descriptor/person detector
+# # initialize the HOG descriptor/person detector
 # hog = cv2.HOGDescriptor()
 # hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
 #
@@ -101,7 +101,7 @@ class PeopleDetector(Resource):
 class PeopleDetector2(Resource):
     def get(self):
         url = request.args.get("url")
-        print("ulr", url)
+        print("url", url)
         response = requests.get(url, stream=True)
         with open('img.png', 'wb') as out_file:
             shutil.copyfileobj(response.raw, out_file)
@@ -113,13 +113,13 @@ class PeopleDetector2(Resource):
         (rects, weights) = hog.detectMultiScale(image, winStride=(4, 4), padding=(8, 8), scale=1.05)
         return {'PeopleCount2': len(rects)}
 
-#https://wallpapercave.com/wp/wp7488219.jpg
-#https://cdn.stocksnap.io/img-thumbs/960w/business-people_TUH3TB1WYX.jpg
+# PRZYKŁADOWE ZDJĘCIE
+# https://thumbs.dreamstime.com/b/enamoured-couple-10989620.jpg
 
 
 api.add_resource(PeopleDetector, '/')
 
-api.add_resource(PeopleDetector2, '/')
+api.add_resource(PeopleDetector2, '/1')
 
 if __name__ == '__main__':
     app.run(debug=True)
