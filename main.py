106522001acb22e89b2d5b8ad6e3d3c82e839302
KaSt94 <k.stanczyk94@gmail.com>
2024-01-21 21:55:48 +0100

    Upload

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import cv2
-from flask import Flask, request
+from flask import Flask, flash, request, redirect, url_for, render_template
 from flask_restful import Resource, Api
 import shutil
 import requests
-
+import os
+from werkzeug.utils import secure_filename
 
 # POKAZYWANIE ZDJĘCIA
 
@@ -25,7 +26,7 @@ import requests
 # hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
 #
 # # load image
-# image = cv2.imread('plaza1.jpg')
+# image = cv2.imread('plaza.jpg')
 # image = cv2.resize(image, (1050, 650))
 #
 # # detect people in the image
@@ -57,11 +58,11 @@ import requests
 #     def get(self):
 #         url = 'https://wallpapercave.com/wp/wp7488219.jpg'
 #         response = requests.get(url, stream=True)
-#         with open('img.png', 'wb') as out_file:
+#         with open('img.jpg', 'wb') as out_file:
 #             shutil.copyfileobj(response.raw, out_file)
 #
 #         # load image
-#         image = cv2.imread('img.png')
+#         image = cv2.imread('img.jpg')
 #
 #         # detect people in the image
 #         (rects, weights) = hog.detectMultiScale(image, winStride=(4, 4), padding=(8, 8), scale=1.05)
@@ -80,14 +81,19 @@ import requests
 hog = cv2.HOGDescriptor()
 hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
 
+UPLOAD_FOLDER = os.path.join('static', 'uploads')
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg',}
+
 app = Flask(__name__)
 api = Api(app)
 
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
 class PeopleDetector(Resource):
     def get(self):
         # load image
-        image = cv2.imread('plaza1.jpg')
+        image = cv2.imread('plaza.jpg')
         image = cv2.resize(image, (1050, 650))
 
         # detect people in the image
@@ -103,26 +109,161 @@ class PeopleDetector2(Resource):
         url = request.args.get("url")
         print("url", url)
         response = requests.get(url, stream=True)
-        with open('img.png', 'wb') as out_file:
+        with open('img.jpg', 'wb') as out_file:
             shutil.copyfileobj(response.raw, out_file)
 
         # load image
-        image = cv2.imread('img.png')
+        image = cv2.imread('img.jpg')
 
         # detect people in the image
         (rects, weights) = hog.detectMultiScale(image, winStride=(4, 4), padding=(8, 8), scale=1.05)
+
         return {'PeopleCount2': len(rects)}
 
 # PRZYKŁADOWE ZDJĘCIE
 # https://thumbs.dreamstime.com/b/enamoured-couple-10989620.jpg
 
 
-api.add_resource(PeopleDetector, '/')
+# ZWRACANIE INFORMACJI HTTPS - WYKRYWANIE LUDZI(PRZESŁANE ZDJĘCIE(wczytane z folderu w przeglądarce)
 
-api.add_resource(PeopleDetector2, '/1')
+class PeopleDetector3(Resource):
+
+    @app.route('/', methods=['GET', 'POST'])
+    def upload_file():
+        if request.method == 'POST':
+            files = request.files.getlist("file[]")
+            filenames = []
+            for file in files:
+                if file:
+                    filename = "Photo.jpg"
+                    file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+                    filenames.append(filename)
+
+            flash("Uploaded files: {}".format(", ".join(filenames)))
+
+        return render_template("file_upload.html")
+
+    def get(self):
+
+        # load image
+        path = r"C:\Python_projekty\Technologie_chmurowe\static\uploads\Photo.jpg"
+        image = cv2.imread(path)
+
+        # detect people in the image
+        (rects, weights) = hog.detectMultiScale(image, winStride=(4, 4), padding=(8, 8), scale=1.05)
+
+        return {'PeopleCount3': len(rects)}
+
+
+api.add_resource(PeopleDetector, '/static')
+
+api.add_resource(PeopleDetector2, '/dynamic')
+
+api.add_resource(PeopleDetector3, '/upload')
 
 if __name__ == '__main__':
     app.run(debug=True)
 
 
-# ZWRACANIE INFORMACJI HTTPS - WYKRYWANIE LUDZI(PRZESŁANE ZDJĘCIE(wczytane z folderu)
+# ZWRACANIE INFORMACJI HTTPS - WYKRYWANIE LUDZI(PRZESŁANE ZDJĘCIE(wczytane z folderu w przeglądarce)
+
+# # initialize the HOG descriptor/person detector
+# hog = cv2.HOGDescriptor()
+# hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+#
+# UPLOAD_FOLDER = os.path.join('static', 'uploads')
+# ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg',}
+#
+# app = Flask(__name__)
+# api = Api(app)
+# app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+# app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+#
+#
+# class PeopleDetector3(Resource):
+#
+#
+#     @app.route('/', methods=['GET', 'POST'])
+#     def upload_file():
+#         if request.method == 'POST':
+#             files = request.files.getlist("file[]")
+#             filenames = []
+#             for file in files:
+#                 if file:
+#                     filename = "Photo.jpg"
+#                     file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+#                     filenames.append(filename)
+#
+#             flash("Uploaded files: {}".format(", ".join(filenames)))
+#
+#         return render_template("file_upload.html")
+#
+#     def get(self):
+#
+#         # load image
+#         path = r"C:\Python_projekty\Technologie_chmurowe\static\uploads\Photo.jpg"
+#         image = cv2.imread(path)
+#
+#         # detect people in the image
+#         (rects, weights) = hog.detectMultiScale(image, winStride=(4, 4), padding=(8, 8), scale=1.05)
+#
+#         return {'PeopleCount3': len(rects)}
+#
+#
+# api.add_resource(PeopleDetector3, '/2')
+#
+#
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
+
+
+# ZWRACANIE INFORMACJI HTTPS - WYKRYWANIE LUDZI(PRZESŁANE ZDJĘCIE(wczytane z folderu w przeglądarce)
+
+# # initialize the HOG descriptor/person detector
+# hog = cv2.HOGDescriptor()
+# hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+#
+# UPLOAD_FOLDER = os.path.join('static', 'uploads')
+# ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg',}
+#
+# app = Flask(__name__)
+# api = Api(app)
+# app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+# app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+#
+#
+# class PeopleDetector3(Resource):
+#
+#
+#     @app.route('/', methods=['GET', 'POST'])
+#     def upload_file():
+#         if request.method == 'POST':
+#             files = request.files.getlist("file[]")
+#             filenames = []
+#             for file in files:
+#                 if file:
+#                     filename = secure_filename(file.filename)
+#                     file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+#                     filenames.append(filename)
+#
+#             flash("Uploaded files: {}".format(", ".join(filenames)))
+#
+#         return render_template("file_upload.html")
+#
+#     def get(self):
+#
+#         # load image
+#         image = cv2.imread()
+#
+#         # detect people in the image
+#         (rects, weights) = hog.detectMultiScale(image, winStride=(4, 4), padding=(8, 8), scale=1.05)
+#
+#         return {'PeopleCount3': len(rects)}
+#
+#
+# api.add_resource(PeopleDetector3, '/2')
+#
+#
+# if __name__ == '__main__':
+#     app.run(debug=True)
